72e412d2afa57257a722f5868511505fe09496e3
Jagadeeshwari1 <mandapallijagadeeshwari@gmail.com>
2026-03-22 10:09:03 -0500

    Update model_loader.py

--- src/model_loader.py
+++ src/model_loader.py
@@ -1,10 +1,1 @@
-import joblib
-from pathlib import Path
 
-def load_model():
-    root = Path(__file__).parents[1]
-    model_path = root / "models" / "cost_predictor.pkl"
-
-    if model_path.exists():
-        return joblib.load(model_path)
-    return None
